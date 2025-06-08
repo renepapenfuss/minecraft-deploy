0739c69d5d0234c9bc4db054fa7b222dfe7343bb
René Papenfuß <rene.papenfuss@live.de>
2025-06-08 21:59:04 +0100

    added a service to deploy resource packs

---
+++ src/services/deploy-resource-pack.servicey.ts
@@ -0,0 +1,106 @@
+import path from "node:path";
+import fs from "node:fs";
+import config from "../config";
+import Rsync from "rsync";
+import { Client } from "ssh2";
+
+interface RemoteOptions {
+  host: string;
+  username: string;
+  remotePath: string;
+  privateKeyPath: string;
+}
+
+export async function deployResourcePack(
+  packName: string,
+  type: string = "development_resource_packs",
+  remoteOptions: RemoteOptions,
+): Promise<void> {
+  console.log(`Deploy resource pack '${packName}' ...`);
+
+  const resourcePackZipPath = path.join(
+    config.minecraftDirectoryPath,
+    type,
+    `${packName}.zip`,
+  );
+
+  await _rsyncToRemote(resourcePackZipPath, remoteOptions);
+  await _runRemoteCommand(
+    `
+    cd "${remoteOptions.remotePath}"
+    ls -la
+  `,
+    remoteOptions,
+  );
+
+  console.log(`Resource pack '${packName}' deployed successfully.`);
+}
+
+function _rsyncToRemote(
+  localFile: string,
+  remoteOptions: RemoteOptions,
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const { username, host, remotePath: path } = remoteOptions;
+    const remote = `${username}@${host}:${path}/`;
+
+    const rsync = new Rsync()
+      .flags("avz")
+      .set("progress")
+      .source(localFile)
+      .destination(remote);
+
+    rsync.execute((error, code) => {
+      if (error) {
+        return reject(new Error(`Rsync failed: ${error.message}`));
+      }
+
+      if (code !== 0) {
+        return reject(new Error(`Rsync exited with code ${code}`));
+      }
+
+      resolve();
+    });
+  });
+}
+
+function _runRemoteCommand(
+  command: string,
+  remoteOptions: RemoteOptions,
+): Promise<void> {
+  const { host, username, privateKeyPath } = remoteOptions;
+
+  let privateKey: Buffer | undefined;
+  if (privateKeyPath) {
+    privateKey = fs.readFileSync(privateKeyPath);
+  }
+
+  return new Promise((resolve, reject) => {
+    const conn = new Client();
+    conn
+      .on("ready", () => {
+        conn.exec(command, (err, stream) => {
+          if (err) return reject(err);
+          stream
+            .on("close", (code: number) => {
+              conn.end();
+              if (code === 0) resolve();
+              else reject(new Error(`Remote command exited with code ${code}`));
+            })
+            .on("data", (data: Uint8Array | string) => {
+              process.stdout.write(data);
+            })
+            .stderr.on("data", (data) => {
+              process.stderr.write(data);
+            });
+        });
+      })
+      .on("error", reject)
+      .connect({
+        host,
+        port: 22,
+        username,
+        privateKey,
+      });
+  });
+}
