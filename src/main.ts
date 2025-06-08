1465db63a3d576c874d7433c5022b3dbeae94bf7
René Papenfuß <rene.papenfuss@live.de>
2025-06-08 21:59:36 +0100

    enable the service to deploy resource packs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Argument, Command, Option } from "commander";
 import { zipBehaviorPack } from "./services/zip-behavior-pack.servive";
 import { zipResourcePack } from "./services/zip-resource-pack.servive";
 import { deployBehaviorPack } from "./services/deploy-behavior-pack.service";
+import { deployResourcePack } from "./services/deploy-resource-pack.servicey";
 
 const program = new Command();
 
@@ -92,8 +93,37 @@ program
           "the name of the resource pack to deploy",
         ).argRequired(),
       )
-      .action(async (packName: string) => {
-        console.log(`Deploying resource pack '${packName}' ...`);
+      .addOption(
+        new Option(
+          "-h, --host <host>",
+          "The host of the remote server",
+        ).makeOptionMandatory(true),
+      )
+      .addOption(
+        new Option(
+          "-u, --username <username>",
+          "The username of the remote server",
+        ).makeOptionMandatory(true),
+      )
+      .addOption(
+        new Option(
+          "-r, --remote-path <remote_path>",
+          "The remote path on the remote server",
+        ).makeOptionMandatory(true),
+      )
+      .addOption(
+        new Option(
+          "-k, --private-key-path <private_key_path>",
+          "The path to the private key for SSH authentication",
+        ).makeOptionMandatory(true),
+      )
+      .action(async (packName: string, remoteOptions) => {
+        await deployResourcePack(packName, "development_resource_packs", {
+          host: remoteOptions.host,
+          username: remoteOptions.username,
+          remotePath: remoteOptions.remotePath,
+          privateKeyPath: remoteOptions.privateKeyPath,
+        });
       }),
   );
 
